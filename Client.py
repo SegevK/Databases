ba427dcb07f7c68c74fcac20ac724b92b2ba2d12
SegevK <55542193+SegevK@users.noreply.github.com>
2019-11-21 14:30:42 +0000

    FINAL(WORKING)

--- Client.py
+++ Client.py
@@ -1,24 +1,45 @@
 import socket
-ip = '0.0.0.0'
+import pickle
+import tkinter as tk
+ip = '127.0.0.1'
 port = 8080
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((ip, port))
-print("please enter username and password")
-name = input("username")
-name = str(name)
-password = input("password")
-password = str(password)
-client.send(name, password)
-data = client.recv(1024)
-if data == "wrong name or password, try again":
-    while data == "wrong name or password, try again":
-        print("please enter username and password")
-        name = input("username")
-        name = str(name)
-        password = input("password")
-        password = str(password)
-        client.send(name, password)
-        data = client.recv(1024)
-# now the client will recive and print all the data from the server, remmember to close socket in the end #
 
+
+def get_data():
+    name = e1.get()
+    name = str(name).encode()
+    password = e2.get()
+    password = str(password).encode()
+    client.send(name)
+    client.send(password)
+    data = client.recv(4096)
+    data = pickle.loads(data)
+    if data == "wrong name or password, try again":
+        while data == "wrong name or password, try again":
+            print("please enter username and password")
+            name = input("username")
+            name = str(name)
+            password = input("password")
+            password = str(password)
+            client.send(name, password)
+            data = client.recv(4096)
+    print(data)
+    master.quit()
+
+
+master = tk.Tk()
+tk.Label(master, text="username").grid(row=0)
+tk.Label(master, text="password").grid(row=1)
+
+e1 = tk.Entry(master)
+e2 = tk.Entry(master)
+
+e1.grid(row=0, column=1)
+e2.grid(row=1, column=1)
+
+tk.Button(master, text='Login', command=get_data).grid(row=3, column=1, sticky=tk.W, pady=4)
+
+tk.mainloop()
 client.close()
--- Server.py
+++ Server.py
@@ -1,30 +1,36 @@
 import socket
 import sqlite3
-ip = '0.0.0.0'
+import pickle
+ip = '127.0.0.1'
 port = 8080
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind((ip, port))
 serv.listen(5)
 conn, addr = serv.accept()
-(user_name, user_password) = serv.recv(1024)
-database = sqlite3.connect('test.db')
-sqlcommand = "SELECT name WHERE name = user_name AND password = user_password"
-lst = sqlcommand.execute   # lst is the list of users with name and password that were sent #
+user_name = conn.recv(1024).decode()
+user_password = conn.recv(1024).decode()
+currser = sqlite3.connect(r"d:\Users\Cyber\Desktop\Databases\Users.db")
+cur = currser.cursor()
+sqlcommand = "SELECT Name FROM users WHERE Name=? AND Password=?"
+lst = cur.execute(sqlcommand, (user_name, user_password,))   # lst is the list of users with name and password that were sent #
 user_exists = False
 for row in lst:
     user_exists = True
 
-
-if  not user_exists:
-    conn.send("wrong name or password, try again")
+if not user_exists:
+    conn.send("wrong name or password, try again").encode()
 
 if user_exists:   # send all data to user, instead of printing it in server
-    cursor = conn.execute("SELECT id, name, address, salary from COMPANY")
+    products = cur.execute("SELECT * from products")
     print("Opened database successfully")
-    for row in cursor:
-        print("ID = ", row[0])
-        print("NAME = ", row[1])
-        print("ADDRESS = ", row[2])
-        print("SALARY = ", row[3], "\n")
+    list_of_products = []
+    for row in products:
+        product = []
+        product.append("Name = " + str(row[0]))
+        product.append("Price = " + str(row[1]))
+        list_of_products.append(product)
+    print(list_of_products)
+    data = pickle.dumps(list_of_products)
+    conn.send(data)
     print("Operation done successfully")
     conn.close()
